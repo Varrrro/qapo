cada6e32def8e021f403d55ce6d8bea12db9f3a9
Victor Vazquez <victorvazrod@gmail.com>
2020-01-31 18:42:12 +0100

    Add baldwin and lamarck variants

---
+++ cmd/lamarck/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"math"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/varrrro/qapo/internal/crossover"
+	"github.com/varrrro/qapo/internal/heuristics"
+	"github.com/varrrro/qapo/internal/mutation"
+	"github.com/varrrro/qapo/internal/qap"
+	"github.com/varrrro/qapo/internal/replacement"
+	"github.com/varrrro/qapo/internal/selection"
+)
+
+const (
+	dataPath = "data/tai256c.dat"
+	nGens    = 1000
+)
+
+func init() {
+	// Set log formatter
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: false,
+	})
+
+	// Write logs to stdout
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	n, w, d, err := qap.ReadData(dataPath)
+	if err != nil {
+		log.WithField("path", dataPath).WithError(err).Fatal("Can't read data file")
+	}
+
+	start := time.Now()
+	pop := qap.RandomPopulation(100, n)
+	qap.CalculateFitness(pop, w, d)
+	for i := 0; i < nGens; i++ {
+		heuristics.HillClimbing(pop, w, d)
+		tmp := selection.Tournament(pop, 10, 100)
+		crossover.Order(tmp)
+		mutation.RandomSwap(tmp)
+		qap.CalculateFitness(tmp, w, d)
+		replacement.Elitist(pop, tmp, 10)
+
+		log.WithFields(log.Fields{
+			"gen":  i,
+			"best": getBest(pop).Fitness,
+		}).Info("Finished generation")
+	}
+
+	elapsed := time.Since(start)
+	log.WithField("seconds", elapsed.Seconds()).Info("Finished execution")
+}
+
+func getBest(perms []*qap.Permutation) *qap.Permutation {
+	f := math.MaxInt64
+	i := -1
+	for j, p := range perms {
+		if p.Fitness < f {
+			f = p.Fitness
+			i = j
+		}
+	}
+	return perms[i]
+}
---
+++ internal/heuristics/hillclimbing.go
@@ -0,0 +1,40 @@
+package heuristics
+
+import (
+	"sync"
+
+	"github.com/varrrro/qapo/internal/qap"
+)
+
+// HillClimbing heuristic algorithm.
+func HillClimbing(perms []*qap.Permutation, w, d [][]int) {
+	var wg sync.WaitGroup
+	for _, p := range perms {
+		wg.Add(1)
+		go func(p *qap.Permutation) {
+			// Create copy of the base permutation
+			tmp := qap.NewPermutation(len(p.Values))
+			copy(tmp.Values, p.Values)
+
+			// Apply hill climbing algorithm
+			for i := range p.Values {
+				for j := range p.Values[i+1:] {
+					tmp.Values[i], tmp.Values[j] = tmp.Values[j], tmp.Values[i]
+					qap.CalculateFitness([]*qap.Permutation{tmp}, w, d)
+
+					if tmp.Fitness < p.Fitness {
+						copy(p.Values, tmp.Values)
+						p.Fitness = tmp.Fitness
+
+						wg.Done()
+						return
+					}
+
+					tmp.Values[i], tmp.Values[j] = tmp.Values[j], tmp.Values[i]
+				}
+			}
+			wg.Done()
+		}(p)
+	}
+	wg.Wait()
+}
